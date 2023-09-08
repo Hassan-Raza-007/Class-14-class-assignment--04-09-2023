14bd5ea99930fb7a72e3fc91d602036886431526
Hassan Raza <hassanbawany65@gmail.com>
2023-09-08 08:40:01 +0500

    add files

---
+++ app.js
@@ -0,0 +1,282 @@
+// Chapter no -1 (alerts)
+
+// Q1
+// alert("Welcome to my Website")
+
+// Q2
+// alert("Error! Please enter a valid Password")
+
+// Q3
+// alert("Welcome to JS Land...  \n  Happy Coding!")
+
+// Q4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+// Q5
+// console.log("Hello... I can run JS through my web browser's ")
+
+// Q6
+// Same
+
+// Chapter no 2 (VARIABLES FOR STRINGS)
+
+// Q1
+// var username;
+
+// Q2
+// var myName = "Hassan Raza"
+// alert(myName)
+
+// Q3
+// var message;
+// message = "Hello World"
+// alert(message)
+
+// Q4
+// var myName = "Hassan Raza"
+// var myAge = "24 Years old"
+// var Des = "Certified Mobile Application Development"
+// alert(myName)
+// alert(myAge)
+// alert(Des)
+
+
+// Q5
+// alert(" PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// Q6
+// alert("my email address is " + "hassanbawany65@gmail.com")
+
+// Q7
+// alert("I am trying to learn from the Book " + "A smarter way to learn JS")
+
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// Q9
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// Chapter no 3 (VARIABLES FOR NUMBERS )
+
+// Q1
+// var age = 24
+// alert("I am " + age + " years old")
+
+// Q2
+// alert("You have visited this site 14 times")
+
+// Q3
+// var birthyear = 1999
+// document.write("my birth year is " + birthyear)
+// document.write("<br>")
+// document.write( "Data type of my declared variabe is " + typeof birthyear)
+
+// Q4
+// var names = "ali asghar"
+// var product = "T-shirt(s)"
+// var quantity = 5
+// document.write(names + " Ordered " + quantity + product + " from xyz store")
+
+// // Chapter no 4 (VARIABLE NAMES: LEGAL & ILLEGAL)
+
+// Q1
+// var num = 10,num2 = 50,num3 = 100
+
+// Q2
+// var names;
+// var num1;
+// var num_num;
+// var num-num;
+// var firstName;
+
+// var First;
+// var 1names;
+// var @names;
+// var $name = "hello";
+// alert($name)
+
+
+// Q3
+// document.write("<b>Rules for naming JS variables</b>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Variable names can only contain <b> $, my_1st,my1 and Variable</b>")
+// document.write("<br>")
+// document.write("Variables must begin with a ______, ______ or_____.<b> For example $name, _name or names</b>")
+// document.write("<br>")
+// document.write("Variable names are case _________")
+// document.write("<br>")
+// document.write("Variable names should not be JS _________")
+
+
+// // Chapter no 5 (MATH EXPRESSIONS)
+
+// Q1
+// var num1 = 3
+// var num2 = 5
+// var total = num1 + num2
+// document.write(total)
+
+
+// Q2
+// var num1 = 3
+// var num2 = 5
+// var total = num1 / num2
+// document.write(total)
+
+// var total = num1 * num2
+// document.write(total)
+
+// var total = num1 - num2
+// document.write(total)
+
+// var total = num1 % num2
+// document.write(total)
+
+// Q3 (a & b)
+// var num3;
+
+// (c)
+// num3 = 5
+// document.write(++num3)
+// document.write("<br>")
+// var inc_val = num3 + 7
+// document.write(inc_val)
+// document.write("<br>")
+// document.write(--inc_val)
+// document.write("<br>")
+// document.write(0 / 3)
+
+// Q4
+// var price = 600
+// var qty = 5
+// document.write("Total cost to buy " + qty  + " tickets to a movie is " + price + "PKR")
+
+// Q5
+// var num1 = 5
+// document.write("table of 5")
+// document.write("<br>")
+// document.write("5X1=" + 5)
+// document.write("<br>")
+// document.write("5X2=" +  5*2)
+// document.write("<br>")
+// document.write("5X3=" +  5*3)
+// document.write("<br>")
+// document.write("5X4=" +  5*4)
+// document.write("<br>")
+// document.write("5X5=" +  5*5)
+// document.write("<br>")
+// document.write("5X6=" +  5*6)
+// document.write("<br>")
+// document.write("5X7=" +  5*7)
+// document.write("<br>")
+// document.write("5X8=" +  5*8)
+// document.write("<br>")
+// document.write("5X9=" +  5*9)
+// document.write("<br>")
+// document.write("5X10=" +  5*10)
+
+// Q6
+// var C = 25
+// document.write("25<sup>o</sup>C is  " + ((C*9/5)+32) + "<sup>o</sup>F")
+// var F = 77
+// document.write("<br>")
+// document.write("77<sup>o</sup>F is  " + ((F-32)* 5/9) + "<sup>o</sup>C")
+
+
+// Q7
+// var item1Price = 650
+// var item1Qty = 3
+// var item2Price = 100
+// var item2Qty =  7
+// var shoppingCharges = 100
+// var total = (item1Price*item1Qty)
+// document.write("<h1>Shopping Cart</h1>")
+// document.write("<br>")
+// document.write("Price of item1 is " + item1Price)
+// document.write("<br>")
+// document.write("Quantity of item1 is " + item1Qty)
+// document.write("<br>")
+// document.write("Price of item2 is " + item2Qty)
+// document.write("<br>")
+// document.write("Quantity of item2 is " + item2Qty)
+// document.write("<br>")
+// document.write("Shopping Charges is " + shoppingCharges)
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total Cost of your order is " + total)
+
+
+// Q8
+// var totalMarks = 980
+// var marksObtain = 804
+// var percentage = marksObtain/totalMarks*100
+//  document.write("<h1> Marks Sheet</h1>")
+//  document.write("<br>")
+//  document.write("<br>")
+//  document.write("Total Marks" + totalMarks)
+//  document.write("<br>")
+//  document.write("Marks Obtain" + marksObtain)
+//  document.write("<br>")
+// document.write(percentage)
+
+
+// Q9
+// var USD = 10
+// var USDtoPKR = 104.8
+// var SAR = 25
+// var SARtoPKR = 28
+// var total = USD*USDtoPKR + SAR*SARtoPKR
+// document.write("<h1>Currecy in PKR</h1>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total Currency in PKR: " + total)
+
+// Q10
+// var num1 = 100
+// var total = (((num1 + 5) * 10) /2) 
+// document.write(total)
+
+// Q11
+// document.write('<h1>Age Calculator</h1>')
+// document.write('<br>')
+// var currentyear = 2023
+// var birthyear = 1999
+// var cal = currentyear - birthyear
+// document.write(cal)
+
+// Q12
+// var radius = 20
+// var pi = 3.142
+// var circumference = radius*pi
+// var area = pi*(radius*2)
+// document.write("<h1>Ther Geometrizer</h1>")
+// document.write("<br>")
+// document.write("radius of a circle is : 20")
+// document.write("<br>")
+// document.write("The circumference is " + circumference)
+// document.write("<br>")
+// document.write("The area is " + area)
+// document.write("<br>")
+
+// Q13
+// var favourite = "Chocolatechip"
+// var currentAge = 24;
+// var estYear = 65;
+// var perdaysnack = 3;
+// var cal = (estYear - currentAge)* (365 * perdaysnack);
+
+// document.write("<h1>The liftime Supply Calculator</h1>")
+// document.write("<br>")
+// document.write("favourite Snack is : " + favourite)
+// document.write("<br>")
+// document.write("Current Age is : " + currentAge)
+// document.write("<br>")
+// document.write("Estimated maximum Age is : " + estYear)
+// document.write("<br>")
+// document.write("Amount of Snack per day : " + perdaysnack)
+// document.write("<br>")
+// document.write("you will need " + cal + "chocolate chip to last you until the ripe old age of" + estYear)
+
